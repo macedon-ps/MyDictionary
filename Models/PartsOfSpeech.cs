b1f60a64a3faef94e3dc1ef510015b28781676d9
macedon-ps <63913255+macedon-ps@users.noreply.github.com>
2024-09-12 15:41:51 +0300

    added new model

---
+++ Models/PartsOfSpeech.cs
@@ -0,0 +1,17 @@
+﻿namespace MyDictionary.Models
+{
+    /// <summary>
+    /// Noun - существительное
+    /// Verb - глагол
+    /// Adjective - прилагательное
+    /// Adverb - наречие 
+    /// Pronoun - местоимение
+    /// Preposition - предлог
+    /// Conjunction - союз
+    /// Interjection - междометие
+    /// </summary>
+    public enum PartsOfSpeech
+    {
+        Noun, Verb, Adjective, Adverb, Pronoun, Preposition, Conjunction, Interjection
+    }
+}
---
+++ Models/Word.cs
@@ -0,0 +1,18 @@
+﻿namespace MyDictionary.Models
+{
+    public class Word
+    {
+        public int Id { get; set; }
+
+        public string RusValue { get; set; } = null!;   
+
+        public string EngValue { get; set; } = null!;
+
+        public string Transcription { get; set; } = "";
+
+        public PartsOfSpeech PartsOfSpeech { get; set; } 
+
+        public Word() { }
+
+    }
+}
