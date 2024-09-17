61444f07b58e1d4f897ee2264ff2d0d8d91a3a2f
macedon-ps <63913255+macedon-ps@users.noreply.github.com>
2024-09-18 02:39:21 +0300

    changed view Error and VM ErrorViewModel

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyDictionary.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ ViewModels/ErrorViewModel.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MyDictionary.ViewModels
+{
+    public class ErrorViewModel
+    {
+        /// <summary>
+        /// Сообщение об ошибке
+        /// </summary>
+        [Display(Name = "Сообщение об ошибке")]
+        public string? ErrorMessage { get; set; }
+
+        /// <summary>
+        /// Дата и время возникновния ошибки
+        /// </summary>
+        [Display(Name = "Дата и время ошибки")]
+        public DateTime DateTimeError { get; set; }
+
+        public ErrorViewModel(string? errorMessage)
+        {
+            DateTimeError = DateTime.Now;
+            ErrorMessage = errorMessage;
+        }
+    }
+}
